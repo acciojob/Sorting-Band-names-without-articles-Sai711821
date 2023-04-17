71cebeae69eddc0fb397b44bbf30b06dc9a9e00d
Sai711821 <117862093+Sai711821@users.noreply.github.com>
2023-04-18 00:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //your code here
 
 let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-arr.sort();
-console.log(arr);
+let updatedList = arr.map((element) => {
+    let words = element.split(" ");
+    let updatedString = words.reduce((prev, current, index) => {
+        if(current != "The" && current != "an" && current != "a"){
+            return prev + " " + current;
+        }
+        return prev;
+    }, "")
+    return updatedString;
+});
+let mp={};
+updatedList.forEach( (element, index) => {
+    mp[element] = arr[index];
+});
+updatedList.sort();
+let finalAns = updatedList.map((element) => {
+    return mp[element] ;
+    
+})
+console.log(finalAns);
